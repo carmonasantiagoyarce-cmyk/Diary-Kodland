78fe19a711233f8387b759d310504fb96e33d5dd
Santiago Carmona Yarce <carmona.santiago.yarce@gmail.com>
2026-04-15 19:12:50 -0500

    Delete PythonProClase1.py

--- PythonProClase1.py
+++
@@ -1,8 +0,0 @@
-import  random
-def gestor(long):
-    caracteres =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    cantidad_caracteres = long
-    almacenar_contrasena = ""
-    for i in range(cantidad_caracteres):
-        almacenar_contrasena += random.choice(caracteres)
-    return (almacenar_contrasena)
